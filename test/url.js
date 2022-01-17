3672a566ea3ec377399f2ec64c9e7566031b7a86
Adrien Burgun <adrien.burgun@orange.fr>
2022-01-17 18:22:57 +0100

    :sparkles: More flairs and fix tests

--- test/url.js
+++ test/url.js
@@ -10,16 +10,20 @@ describe("Test confirmed scam links", () => {
         assert(!(await validate_message(url, true))[0]);
     }
 
-    it("Simple links", async () => {
-        for (let n = 0; n < 100; n++) {
-            await assertBad("https://dlscord-new-year.ru.com/gw20HkJ5qmqG13");
+    it("Warmup", async () => {
+        await assertGood("Hello world");
+    });
 
-            await assertGood("dlscord links are so spammy");
-            await assertBad(
-                `@everyone Hi Steam gives nitro gifts for 3 months for the new year\n` +
-                `https://dlscord-new-year.ru.com/gw20HkJ5qmqG13 Only the account must not be empty, otherwise it will not be given\n` +
-                `If they give a gift for a long time, then just wait. Sometimes you have to wait up to 2 days`
-            );
-        }
+    it("Simple links", async () => {
+        await assertBad("https://dlscord-new-year.ru.com/gw20HkJ5qmqG13");
+        await assertBad("https://discoqd.com/newyear");
+
+        await assertGood("dlscord links are so spammy");
+
+        await assertBad(
+            `@everyone Hi Steam gives nitro gifts for 3 months for the new year\n` +
+            `https://dlscord-new-year.ru.com/gw20HkJ5qmqG13 Only the account must not be empty, otherwise it will not be given\n` +
+            `If they give a gift for a long time, then just wait. Sometimes you have to wait up to 2 days`
+        );
     });
 });
